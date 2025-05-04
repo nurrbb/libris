0f7b47605e09e9b8593f66a5231a337eeee44577
nurrbb <enurb320@gmail.com>
2025-05-04 14:08:19 +0300

    rename BookResponseDTO to BookResponse for clarity and brevity

---
+++ src/main/java/com/nurbb/libris/model/dto/response/BookResponse.java
@@ -0,0 +1,20 @@
+package com.nurbb.libris.model.dto.response;
+
+import com.nurbb.libris.model.entity.Genre;
+
+import java.time.LocalDate;
+import java.util.Set;
+import java.util.UUID;
+
+public record BookResponse(
+        UUID id,
+        String title,
+        Set<String> authors,
+        String isbn,
+        LocalDate publishedDate,
+        Genre genre,
+        Integer count,
+        boolean isAvailable,
+        String createdAt,
+        String updatedAt
+) {}
