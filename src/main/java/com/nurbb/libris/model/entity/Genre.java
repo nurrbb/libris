2ba5069ba1cb1a21ec4e5d0b8d49f6066cda6222
nurrbb <enurb320@gmail.com>
2025-05-04 12:40:25 +0300

    add Genre enum for book categorization

---
+++ src/main/java/com/nurbb/libris/model/entity/Genre.java
@@ -0,0 +1,16 @@
+package com.nurbb.libris.model.entity;
+
+public enum Genre {
+    FICTION,
+    NON_FICTION,
+    SCIENCE,
+    HISTORY,
+    ROMANCE,
+    FANTASY,
+    BIOGRAPHY,
+    MYSTERY,
+    HORROR,
+    ART,
+    TECHNOLOGY,
+    OTHER
+}
