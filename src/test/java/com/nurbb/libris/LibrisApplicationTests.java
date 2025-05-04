d11b90a7c641962d124c24df89f85e9a2273df3e
nurrbb <enurb320@gmail.com>
2025-05-04 09:41:31 +0300

    add basic Spring Boot context load test

---
+++ src/test/java/com/nurbb/libris/LibrisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurbb.libris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
