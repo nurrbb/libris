79c5870dc2d250031e14d0f56362fde7c6de7ba0
nurrbb <enurb320@gmail.com>
2025-05-04 13:55:19 +0300

    implement AuthorMapper for converting between Author entity and DTOs

---
+++ src/main/java/com/nurbb/libris/model/mapper/AuthorMapper.java
@@ -0,0 +1,20 @@
+package com.nurbb.libris.model.mapper;
+
+import com.nurbb.libris.model.dto.request.AuthorRequest;
+import com.nurbb.libris.model.dto.response.AuthorResponse;
+import com.nurbb.libris.model.entity.Author;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = "spring", imports = {UUID.class, Collectors.class})
+public interface AuthorMapper {
+
+    @Mapping(target = "id", expression = "java(UUID.randomUUID())")
+    Author toEntity(AuthorRequest request);
+
+    @Mapping(target = "books", expression = "java(author.getBooks().stream().map(Book::getTitle).collect(Collectors.toSet()))")
+    AuthorResponse toResponse(Author author);
+}
