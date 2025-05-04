7e88bbf28c0bdcb89598379bea1452d0522a4669
nurrbb <enurb320@gmail.com>
2025-05-04 12:40:35 +0300

    enable JPA auditing for BaseEntity audit fields

--- src/main/java/com/nurbb/libris/LibrisApplication.java
+++ src/main/java/com/nurbb/libris/LibrisApplication.java
@@ -2,8 +2,10 @@ package com.nurbb.libris;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LibrisApplication {
 
     public static void main(String[] args) {
