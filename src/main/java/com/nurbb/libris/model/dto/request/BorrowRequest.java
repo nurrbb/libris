0a28305bc6d0c7c97f766715a73b9704a5acd267
nurrbb <enurb320@gmail.com>
2025-05-04 13:30:50 +0300

    feat: add BorrowRequest DTO for initiating book borrow operations

---
+++ src/main/java/com/nurbb/libris/model/dto/request/BorrowRequest.java
@@ -0,0 +1,27 @@
+package com.nurbb.libris.model.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BorrowRequest {
+
+    @NotNull
+    private UUID bookId; //name?
+
+    @NotNull
+    private UUID userId; //name?
+
+    @NotNull
+    private LocalDate borrowDate;
+
+    @NotNull
+    private LocalDate dueDate;
+}
