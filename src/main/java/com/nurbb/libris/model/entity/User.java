bc78effff1c46c6d85f655fd8460f132b2166cdc
nurrbb <enurb320@gmail.com>
2025-05-04 12:41:05 +0300

    create User entity with role and soft delete support

---
+++ src/main/java/com/nurbb/libris/model/entity/User.java
@@ -0,0 +1,38 @@
+package com.nurbb.libris.model.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class User extends BaseEntity {
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    @Email
+    private String email;
+
+    @Size(min = 5, message = "Minimum password length: 5 characters")
+    @Column(nullable = false)
+    private String password;
+
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    private Boolean deleted = false;
+}
