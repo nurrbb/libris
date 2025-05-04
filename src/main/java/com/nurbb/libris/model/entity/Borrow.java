e144610207e5bb69d1ecacb21326679885027b6a
nurrbb <enurb320@gmail.com>
2025-05-04 12:40:16 +0300

    define Borrow entity to track book lending with user and book relations

---
+++ src/main/java/com/nurbb/libris/model/entity/Borrow.java
@@ -0,0 +1,37 @@
+package com.nurbb.libris.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "borrows")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Borrow extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "borrow_date", nullable = false)
+    private LocalDate borrowDate;
+
+    @Column(name = "due_date", nullable = false)
+    private LocalDate dueDate;
+
+    @Column(name = "return_date") //getirme tarihi girilecek mi?
+    private LocalDate returnDate;
+
+    @Column(nullable = false)
+    private Boolean returned = false;
+
+}
