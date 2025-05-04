87056e251a4af42ce1461f901d49ffa6148df219
nurrbb <enurb320@gmail.com>
2025-05-04 13:30:59 +0300

    feat: add BorrowResponseDTO to return borrow record information

---
+++ src/main/java/com/nurbb/libris/model/dto/response/BorrowResponseDTO.java
@@ -0,0 +1,17 @@
+package com.nurbb.libris.model.dto.response;
+
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record BorrowResponseDTO(
+        UUID id,
+        String bookTitle,
+        String userFullName,
+        LocalDate borrowDate,
+        LocalDate dueDate,
+        LocalDate returnDate,
+        boolean returned,
+        String createdAt,
+        String updatedAt
+) {}
