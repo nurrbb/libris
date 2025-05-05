eefc10ea9859df7ed356a1fd9cecdc8f475168a4
nurrbb <enurb320@gmail.com>
2025-05-05 18:32:44 +0300

    feat(core): create auditable BaseEntity with UUID and timestamp fields

--- src/main/java/com/nurbb/libris/model/entity/BaseEntity.java
+++ src/main/java/com/nurbb/libris/model/entity/BaseEntity.java
@@ -1,21 +1,21 @@
 package com.nurbb.libris.model.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @MappedSuperclass
-//@EntityLister
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
+@NoArgsConstructor
 public class BaseEntity {
 
 
