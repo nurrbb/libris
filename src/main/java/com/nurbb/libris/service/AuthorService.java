438d4374dc6c79c8beba622b6f6e0209666e3102
nurrbb <enurb320@gmail.com>
2025-05-05 18:32:08 +0300

    feat(author): define AuthorService interface for business logic abstraction

---
+++ src/main/java/com/nurbb/libris/service/AuthorService.java
@@ -0,0 +1,20 @@
+package com.nurbb.libris.service;
+
+import com.nurbb.libris.model.entity.Author;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface AuthorService {
+    Author createAuthor(String name);
+
+    List<Author> getAllAuthors();
+
+    Optional<Author> getAuthorById(UUID id);
+
+    Optional<Author> getAuthorByName(String name);
+
+    Author getAuthorByNameOrCreate(String name);
+
+}
