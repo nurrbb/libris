225717878ea21a5b9830a306185bf9bf2fc54961
nurrbb <enurb320@gmail.com>
2025-05-05 18:33:12 +0300

    feat(book): add BookMapper for request/response transformations

--- src/main/java/com/nurbb/libris/model/mapper/BookMapper.java
+++ src/main/java/com/nurbb/libris/model/mapper/BookMapper.java
@@ -2,40 +2,24 @@ package com.nurbb.libris.model.mapper;
 
 import com.nurbb.libris.model.dto.request.BookRequest;
 import com.nurbb.libris.model.dto.response.BookResponse;
-import com.nurbb.libris.model.entity.Book;
 import com.nurbb.libris.model.entity.Author;
+import com.nurbb.libris.model.entity.Book;
 import org.mapstruct.*;
 
-import java.util.Set;
-import java.util.stream.Collectors;
 @Mapper(componentModel = "spring")
 public interface BookMapper {
 
-    @Mapping(target = "authors", source = "authors", qualifiedByName = "mapAuthorNamesToEntities")
+    @Mapping(target = "author", expression = "java(author)")
     @Mapping(target = "isAvailable", ignore = true)
-    Book toEntity(BookRequest request, @Context Set<Author> authorEntities);
+    Book toEntity(BookRequest request, @Context Author author);
 
     @AfterMapping
     default void setAvailability(@MappingTarget Book book, BookRequest request) {
         book.setAvailable(request.getCount() != null && request.getCount() > 0);
     }
 
-    @Mapping(target = "authorNames", source = "authors", qualifiedByName = "mapAuthorsToNames")
+    @Mapping(target = "authorName", source = "author.name")
     @Mapping(target = "createdAt", expression = "java(book.getCreatedDate().toString())")
     @Mapping(target = "updatedAt", expression = "java(book.getUpdatedDate().toString())")
     BookResponse toResponse(Book book);
-
-    @Named("mapAuthorsToNames")
-    static Set<String> mapAuthorsToNames(Set<Author> authors) {
-        return authors.stream()
-                .map(Author::getName)
-                .collect(Collectors.toSet());
-    }
-
-    @Named("mapAuthorNamesToEntities")
-    static Set<Author> mapAuthorNamesToEntities(Set<String> names, @Context Set<Author> authorEntities) {
-        return authorEntities.stream()
-                .filter(author -> names.contains(author.getName()))
-                .collect(Collectors.toSet());
-    }
 }
