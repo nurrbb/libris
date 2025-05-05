cb760cfad862311bb85cd9717cf1df350dcdc3de
nurrbb <enurb320@gmail.com>
2025-05-05 18:32:00 +0300

    feat(author): create AuthorResponse DTO for exposing author data

--- src/main/java/com/nurbb/libris/model/dto/response/AuthorResponse.java
+++ src/main/java/com/nurbb/libris/model/dto/response/AuthorResponse.java
@@ -1,11 +1,17 @@
 package com.nurbb.libris.model.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Set;
 import java.util.UUID;
 
-public record AuthorResponse(
-        UUID id,
-        String name,
-        Set<String> books
-) {}
-//
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthorResponse {
+    private UUID id;
+    private String name;
+    private Set<String> books;
+}
