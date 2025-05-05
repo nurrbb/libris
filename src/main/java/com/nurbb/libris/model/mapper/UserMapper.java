00d42f057437058dbaf53b0fb46fc2ef81824f95
nurrbb <enurb320@gmail.com>
2025-05-05 18:39:07 +0300

    feat(user): add UserMapper to convert between entity and DTOs using MapStruct

--- src/main/java/com/nurbb/libris/model/mapper/UserMapper.java
+++ src/main/java/com/nurbb/libris/model/mapper/UserMapper.java
@@ -2,17 +2,21 @@ package com.nurbb.libris.model.mapper;
 
 import com.nurbb.libris.model.dto.request.UserRequest;
 import com.nurbb.libris.model.dto.response.UserResponse;
+import com.nurbb.libris.model.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.security.core.userdetails.User;
+
 
 import java.util.UUID;
 
 @Mapper(componentModel = "spring", imports = {UUID.class})
 public interface UserMapper {
 
+
     @Mapping(target = "id", expression = "java(UUID.randomUUID())")
     @Mapping(target = "deleted", constant = "false")
+    @Mapping(target = "createdDate", ignore = true)
+    @Mapping(target = "updatedDate", ignore = true)
     User toEntity(UserRequest request);
 
     @Mapping(target = "createdAt", expression = "java(user.getCreatedDate().toString())")
