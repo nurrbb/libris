f68c870a77537341a272306b955ecf362569f947
nurrbb <enurb320@gmail.com>
2025-05-05 18:34:46 +0300

    feat(book): create BookResponse DTO for exposing book data

--- src/main/java/com/nurbb/libris/model/dto/response/BookResponse.java
+++ src/main/java/com/nurbb/libris/model/dto/response/BookResponse.java
@@ -1,20 +1,24 @@
 package com.nurbb.libris.model.dto.response;
 
 import com.nurbb.libris.model.entity.Genre;
+import lombok.Data;
 
 import java.time.LocalDate;
-import java.util.Set;
 import java.util.UUID;
 
-public record BookResponse(
-        UUID id,
-        String title,
-        Set<String> authors,
-        String isbn,
-        LocalDate publishedDate,
-        Genre genre,
-        Integer count,
-        boolean isAvailable,
-        String createdAt,
-        String updatedAt
-) {}
+@Data
+public class BookResponse {
+
+    private UUID id;
+    private String title;
+    private String authorName;
+
+    private String isbn;
+    private LocalDate publishedDate;
+    private Genre genre;
+    private Integer count;
+    private boolean isAvailable;
+
+    private String createdAt;
+    private String updatedAt;
+}
