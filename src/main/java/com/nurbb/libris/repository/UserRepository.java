8b73038a2c0a0f66fbc3cafafc692d3610db36f4
nurrbb <enurb320@gmail.com>
2025-05-05 18:39:15 +0300

    feat(user): create JPA repository interface for User entity

---
+++ src/main/java/com/nurbb/libris/repository/UserRepository.java
@@ -0,0 +1,24 @@
+package com.nurbb.libris.repository;
+
+import com.nurbb.libris.model.entity.Role;
+import com.nurbb.libris.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmailIgnoreCase(String email);
+
+    boolean existsByEmailIgnoreCase(String email);
+
+    List<User> findByRole(Role role);
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByIdAndDeletedFalse(UUID id);
+}
