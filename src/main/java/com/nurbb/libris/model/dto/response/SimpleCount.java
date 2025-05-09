c5b241d3df8877320c5732113deedd8d6ab1f45d
nurrbb <enurb320@gmail.com>
2025-05-09 12:05:40 +0300

    Add DTO for generic count-based statistics aggregation

---
+++ src/main/java/com/nurbb/libris/model/dto/response/SimpleCount.java
@@ -0,0 +1,12 @@
+package com.nurbb.libris.model.dto.response;
+
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SimpleCount {
+    private String name;
+    private long count;
+}
