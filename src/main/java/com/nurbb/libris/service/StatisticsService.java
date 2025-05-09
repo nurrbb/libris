bfff392cb51570d0345a1c7ae10d7a14b6039d8b
nurrbb <enurb320@gmail.com>
2025-05-09 12:05:55 +0300

    Define service interface for collecting and aggregating borrow statistics

---
+++ src/main/java/com/nurbb/libris/service/StatisticsService.java
@@ -0,0 +1,11 @@
+package com.nurbb.libris.service;
+
+import com.nurbb.libris.model.dto.response.LibraryStatisticsResponse;
+
+import java.util.Map;
+
+public interface StatisticsService {
+    LibraryStatisticsResponse getLibraryStatistics();
+    Map<String, Object> getOverdueBookStatistics();
+
+}
