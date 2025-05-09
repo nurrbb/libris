a2daafe048ee91b05268bc2d64ab0c735a7d4e07
nurrbb <enurb320@gmail.com>
2025-05-09 12:07:58 +0300

    Add UserStatisticsResponse DTO to encapsulate score and level analytics

---
+++ src/main/java/com/nurbb/libris/model/dto/response/UserStatisticsResponse.java
@@ -0,0 +1,25 @@
+package com.nurbb.libris.model.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class UserStatisticsResponse {
+
+    private String email;
+    private String level;
+    private int score;
+
+    private int totalBorrowedBooks;
+    private int totalReturnedBooks;
+    private int totalLateReturns;
+
+    private int totalReadingDays;
+    private int totalReadPages;
+    private double avgPagesPerDay;
+    private double avgReturnDuration;
+
+}
