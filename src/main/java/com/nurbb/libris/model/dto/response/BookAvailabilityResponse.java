cc65075a8ee9fa065a1935150f55cfaf4166470e
nurrbb <enurb320@gmail.com>
2025-05-10 21:16:35 +0300

    chore(dto): Create BookAvailabilityResponse DTO for SSE stream

---
+++ src/main/java/com/nurbb/libris/model/dto/response/BookAvailabilityResponse.java
@@ -0,0 +1,15 @@
+package com.nurbb.libris.model.dto.response;
+
+import lombok.*;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookAvailabilityResponse {
+    private UUID bookId;
+    private boolean isAvailable;
+    private String title;
+}
