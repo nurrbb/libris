04a8606d1db6523337caaea540c5cefb5195917a
nurrbb <enurb320@gmail.com>
2025-05-10 21:17:29 +0300

    chore(entity): Define borrow entity for tracking lending activity

--- src/main/java/com/nurbb/libris/model/entity/Borrow.java
+++ src/main/java/com/nurbb/libris/model/entity/Borrow.java
@@ -26,7 +26,7 @@ public class Borrow extends BaseEntity {
     @Column(name = "borrow_date", nullable = false)
     private LocalDate borrowDate;
 
-    @Column(name = "due_date", nullable = false)
+    @Column(name = "due_date")
     private LocalDate dueDate;
 
 
