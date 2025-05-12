cd1cb484e7fcda01115996d0b479d8ab1315625b
nurrbb <enurb320@gmail.com>
2025-05-12 18:13:51 +0300

    refactor(service): define contract for statistics calculations

--- src/main/java/com/nurbb/libris/service/StatisticsService.java
+++ src/main/java/com/nurbb/libris/service/StatisticsService.java
@@ -5,6 +5,7 @@ import com.nurbb.libris.model.dto.response.LibraryStatisticsResponse;
 import java.util.Map;
 
 public interface StatisticsService {
+
     LibraryStatisticsResponse getLibraryStatistics();
     Map<String, Object> getOverdueBookStatistics();
 
