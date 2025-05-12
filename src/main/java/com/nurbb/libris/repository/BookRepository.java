e4b61cec4427efeb378610dc44ec8632b1c39b6f
nurrbb <enurb320@gmail.com>
2025-05-12 18:05:40 +0300

    refactor(repository): remove unused query methods and align with service logic

--- src/main/java/com/nurbb/libris/repository/BookRepository.java
+++ src/main/java/com/nurbb/libris/repository/BookRepository.java
@@ -1,25 +1,19 @@
 package com.nurbb.libris.repository;
 
 import com.nurbb.libris.model.entity.Book;
-import com.nurbb.libris.model.entity.Genre;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, UUID> {
 
-    Optional<Book> findByIsbn(String isbn);
-
     boolean existsByIsbn(String isbn);
 
     Page<Book> findByTitleContainingIgnoreCase(String title, Pageable pageable);
-
-    Page<Book> findByGenre(Genre genre, Pageable pageable);
 
     Page<Book> findByIsbnContainingIgnoreCase(String isbn, Pageable pageable);
 
