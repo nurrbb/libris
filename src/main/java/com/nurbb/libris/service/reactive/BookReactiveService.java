641694ca8feef49f62f8546c3f6862f12f47064e
nurrbb <enurb320@gmail.com>
2025-05-12 18:16:37 +0300

    feat(reactive): add reactive service logic using WebFlux for dynamic book queries

---
+++ src/main/java/com/nurbb/libris/service/reactive/BookReactiveService.java
@@ -0,0 +1,21 @@
+package com.nurbb.libris.service.reactive;
+
+
+import com.nurbb.libris.model.dto.response.BookResponse;
+import com.nurbb.libris.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+@Service
+@RequiredArgsConstructor
+public class BookReactiveService {
+
+    private final BookService bookService;
+
+    public Flux<BookResponse> searchBooksReactively(String query, int page, int size) {
+        Page<BookResponse> pageResult = bookService.searchBooks(query, page, size);
+        return Flux.fromIterable(pageResult.getContent());
+    }
+}
